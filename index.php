978ecaad69db7a743fdbcf25dc62021f272c3e02
Amadare <amxdxre@gmail.com>
2026-01-13 20:15:59 +0300

    Добавлен новый путь в котором передаётся имя контроллера, обработчик API.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Cocktails\CocktailController;
+use Cocktails\Controller\API;
 use Cocktails\IngredientController;
 
 include "vendor/autoload.php";
@@ -9,6 +10,7 @@ include "generated-conf/config.php";
 $dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $route) {
     $route->addRoute('GET', '/cocktails', [CocktailController::class, 'renderPage']);
     $route->addRoute('GET', '/ingredients', [IngredientController::class, 'renderPage']);
+    $route->addRoute('POST', '/api/{controller}', [API::class, 'processPost']);
 });
 
 $httpMethod = $_SERVER['REQUEST_METHOD'];
@@ -26,11 +28,15 @@ switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::FOUND:
         $handler = $routeInfo[1];
         $vars = $routeInfo[2];
-//        $controller = $vars['controller'];
         $controllerClass = $handler[0];
         $controller = new $controllerClass;
         $method = $handler[1];
-//        $inputJSON = file_get_contents('php://input');
+        if ($handler[0] === API::class) {
+            $APIcontroller = $vars['controller'];
+            $rawBody = file_get_contents('php://input');
+            call_user_func_array([$controller, $method], [$APIcontroller, $rawBody]);
+            break;
+        }
         call_user_func_array([$controller, $method], []);
         break;
 }
